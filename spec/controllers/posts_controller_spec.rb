2dab0ec92b879c1cb944bae208a47f8f3996c4e8
jyapujuju <danieldangol@gmail.com>
2016-10-25 16:50:11 +0545

    delete rspec

--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -84,4 +84,19 @@ RSpec.describe PostsController, type: :controller do
       end
   end
 
+  describe "Delete #destroy" do
+    it "delte the post" do
+      post = FactoryGirl.create(:post)
+      expect {
+        delete :destroy, id: post.id
+      }.to change(Post, :count).by(-1) 
+    end
+
+    it "redirect to root_path"do
+      post = FactoryGirl.create(:post)
+      delete :destroy, id: post.id
+      expect(response).to redirect_to root_path
+    end
+  end
+
 end
