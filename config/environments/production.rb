fa05df1a9f060acfc6d10bed80034cc2b83adb2d
jyapujuju <danieldangol@gmail.com>
2016-10-27 17:20:43 +0545

    add the assets in production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,6 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
-  config.serve_static_assets = true
   config.assets.compile = true
   config.assets.digest = true
 
